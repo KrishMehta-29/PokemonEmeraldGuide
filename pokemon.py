d336c189bc640bcb71b1b048693fb1bf98aa6599
KrishMehta-29 <61756865+KrishMehta-29@users.noreply.github.com>
2023-02-25 15:38:58 -0800

    Finished Mining all Pokemon
    Mined Pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,25 +1,54 @@
 import requests
 import json
+import csv
 
-BASE_URL = "https://pokeapi.co/api/v2/"
-POKEDEX = "pokedex/4/"
+with open('csvs/pokemons.csv', 'w',  newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["DexID", "Name", "bst", "type1", "type2"])
 
-res = requests.get(BASE_URL + POKEDEX)
-json_object = res.json()
+    BASE_URL = "https://pokeapi.co/api/v2/"
+    POKEDEX = "pokedex/4/"
 
-pokemonSpecies = json_object["pokemon_entries"]
+    res = requests.get(BASE_URL + POKEDEX)
+    json_object = res.json()
 
-pokemonLst = []
+    pokemonSpecies = json_object["pokemon_entries"]
 
-for pokemonS in pokemonSpecies:
-    res = requests.get(pokemonS['pokemon_species']['url'])
+    pokemonLst = []
+    i = 0
 
-    pokemons = res.json()['varieties']
+    for pokemonS in pokemonSpecies:
+        res = requests.get(pokemonS['pokemon_species']['url'])
 
-    # SPRITES CAN BE GOT
-    
-    for pokemon in pokemons:
-        res = requests.get(pokemon['pokemon']['url'])
-        print(json.dumps(res.json(), indent=2))
-    
-    break
+        pokemons = res.json()['varieties']
+
+        # SPRITES CAN BE GOT
+        
+        for pokemon in pokemons:
+            if not pokemon['is_default']:
+                continue
+
+            res = requests.get(pokemon['pokemon']['url'])
+            r = res.json()
+            stats = r['stats']
+            types = r['types']
+            name = r['name']
+
+            bst = 0
+            for stat in stats:
+                bst += int(stat['base_stat'])
+
+            ts = []
+            for type in types:
+            
+                ts.append(type['type']['name'])
+
+            toWrite = [r['id'], r['name'], str(bst)] + ts
+            writer.writerow(toWrite)
+      
+
+        i += 1
+        if i % 10 == 0: print(i)
+
+        if i > 5:
+            break
