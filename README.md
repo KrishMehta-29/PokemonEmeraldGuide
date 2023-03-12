13ef29e7c86804897a88b314bed04da4e6ed0e4f
Leo Yang <leoyang33@yahoo.com>
2023-03-12 13:13:30 -0700

    password authentication

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # PokemonEmeraldGuide
 
+### Data Collection
+Our data primarily comes from scraping [PokeAPI](https://pokeapi.co/) using custom Python scripts.
+We make a lot of API calls to gather all the information neeeded about the Pokemon involved, type matchups,
+evolutions, and routes.
+
 ### Database Setup Instructions
-1. `sudo mysql --local-infile=1 -u root -p`
+1. `sudo /etc/init.d/mysql start` then `sudo mysql --local-infile=1 -u root -p` to start MySQL server
 2. `set global local_infile=true;`
-3. Create MySQL database to store info in
-4. `source setup-db.sql;`
-5. `source load-db.sql;`
+3. `SET GLOBAL log_bin_trust_function_creators = 1;`
+4. Create MySQL database to store info in, e.g. `create database pkmnemeralddb`, `use pkmnemeralddb`
+5. `source setup-db.sql;`
+6. `source setup-passwords.sql`
+7. `source load-db.sql;`
---
+++ setup-passwords.sql
@@ -0,0 +1,105 @@
+-- File for Password Management section of Final Project
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+-- (Provided) This function generates a specified number of characters for using as a
+-- salt in passwords.
+DELIMITER !
+CREATE FUNCTION make_salt(num_chars INT) 
+RETURNS VARCHAR(20) NOT DETERMINISTIC
+BEGIN
+    DECLARE salt VARCHAR(20) DEFAULT '';
+
+    -- Don't want to generate more than 20 characters of salt.
+    SET num_chars = LEAST(20, num_chars);
+
+    -- Generate the salt!  Characters used are ASCII code 32 (space)
+    -- through 126 ('z').
+    WHILE num_chars > 0 DO
+        SET salt = CONCAT(salt, CHAR(32 + FLOOR(RAND() * 95)));
+        SET num_chars = num_chars - 1;
+    END WHILE;
+
+    RETURN salt;
+END !
+DELIMITER ;
+
+-- Provided (you may modify if you choose)
+-- This table holds information for authenticating users based on
+-- a password.  Passwords are not stored plaintext so that they
+-- cannot be used by people that shouldn't have them.
+-- You may extend that table to include an is_admin or role attribute if you 
+-- have admin or other roles for users in your application 
+-- (e.g. store managers, data managers, etc.)
+CREATE TABLE user_info (
+    -- Usernames are up to 20 characters.
+    username VARCHAR(20) PRIMARY KEY,
+
+    -- Salt will be 8 characters all the time, so we can make this 8.
+    salt CHAR(8) NOT NULL,
+
+    -- We use SHA-2 with 256-bit hashes.  MySQL returns the hash
+    -- value as a hexadecimal string, which means that each byte is
+    -- represented as 2 characters.  Thus, 256 / 8 * 2 = 64.
+    -- We can use BINARY or CHAR here; BINARY simply has a different
+    -- definition for comparison/sorting than CHAR.
+    password_hash BINARY(64) NOT NULL
+);
+
+-- [Problem 1a]
+-- Adds a new user to the user_info table, using the specified password (max
+-- of 20 characters). Salts the password with a newly-generated salt value,
+-- and then the salt and hash values are both stored in the table.
+DELIMITER !
+CREATE PROCEDURE sp_add_user(new_username VARCHAR(20), password VARCHAR(20))
+BEGIN
+    DECLARE salt CHAR(8) DEFAULT '';
+
+    SELECT make_salt(8) INTO salt;
+
+    INSERT INTO user_info VALUES (new_username, salt, SHA2(CONCAT(salt, password), 256));
+END !
+DELIMITER ;
+
+-- [Problem 1b]
+-- Authenticates the specified username and password against the data
+-- in the user_info table.  Returns 1 if the user appears in the table, and the
+-- specified password hashes to the value for the user. Otherwise returns 0.
+DELIMITER !
+CREATE FUNCTION authenticate(username1 VARCHAR(20), password VARCHAR(20))
+RETURNS TINYINT DETERMINISTIC
+BEGIN
+    DECLARE s CHAR(8);
+    DECLARE pw BINARY(64);
+
+    SELECT password_hash INTO pw
+    FROM user_info
+    WHERE username = username1;
+
+    SELECT salt INTO s
+    FROM user_info
+    WHERE username = username1;
+
+    IF SHA2(CONCAT(s, password), 256) = pw THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
+END !
+DELIMITER ;
+
+-- [Problem 1c]
+-- Add at least two users into your user_info table so that when we run this file,
+-- we will have examples users in the database.
+CALL sp_add_user('bob', 'password');
+CALL sp_add_user('bill', 'dog');
+
+SELECT authenticate('bob', 'hello');     -- Should return 0 (false)
+SELECT authenticate('bob', 'password');    -- Should return 1 (true)
+SELECT authenticate('bob', 'dog');      -- Should return 0 (false)
+SELECT authenticate('bill', 'password');      -- Should return 0 (false)
+SELECT authenticate('bill', 'dog');   -- Should return 1 (true)
+
+-- [Problem 1d]
+-- Optional: Create a procedure sp_change_password to generate a new salt and change the given
+-- user's password to the given password (after salting and hashing)
